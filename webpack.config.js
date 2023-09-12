4a2f26135f76a6c363441f69120e8133134d8611
Янбухтин Максим <myanbukhtin@gmail.com>
2023-09-12 23:29:50 +0300

    check

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,46 @@
-import { fileURLToPath } from 'url';
-import CopyPlugin from 'copy-webpack-plugin';
-import HtmlPlugin from 'html-webpack-plugin';
-import { dirname, resolve } from 'path';
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export default {
+module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.[contenthash].js',
-    path: resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'build'),
     clean: true,
   },
-  devtool: 'source-map',
-  plugins: [
-    new HtmlPlugin({
-      template: 'public/index.html',
-    }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: 'public',
-          globOptions: {
-            ignore: ['**/index.html'],
-          },
-        },
-      ],
-    }),
-  ],
-  module: {
-    rules: [
+devtool: 'source-map',
+plugins: [
+  new HtmlPlugin({
+    template: 'public/index.html',
+  }),
+  new CopyPlugin({
+    patterns: [
       {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          },
+        from: 'public',
+        globOptions: {
+          ignore: ['**/index.html'],
         },
       },
+    ],
+  }),
+],
+module: {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        },
+      },
+    },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
       }
-    ]
-  }
+    ],
+  },
 };
